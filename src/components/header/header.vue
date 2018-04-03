9526452bd367e2758559a989a15c853b8d4e882b
fulong <834424581@qq.com>
2018-04-03 15:38:39 +0800

    组件拆分并应用stylus语法并安装对应模块

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,11 +1,13 @@
 <template>
-
+<div class="header">
+  我是header
+</div>
 </template>
 
 <script>
     export default {
-        name: "header"
-    }
+        name: 'header'
+    };
 </script>
 
 <style scoped>
