0d2da978f6f5e53d0647ff092f23df39847e179f
fulong <834424581@qq.com>
2018-04-08 13:56:59 +0800

    添加弹出浮层并添加star组件并添加小图标

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -247,6 +247,43 @@
               padding: 0 12px
               font-weight: 700
               font-size: 14px
+          .supports
+            width: 80%
+            margin: 0 auto
+            .support-item
+              padding: 0 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                line-height: 16px
+                font-size: 12px
+            .bulletin
+              width: 80%
+              margin: 0 auto
+              .content
+                padding: 0 12px
+                line-height: 24px
+                font-size: 12px
       .detail-close
         position: relative
         width: 32px
